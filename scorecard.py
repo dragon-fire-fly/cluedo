4dfbd461eb8321ffcba4ce2f1f70523b87efdb7f
Emily Knight <ek93@hotmail.co.uk>
2022-10-27 14:39:18 +0200

    Add annotations and remove unneccessary code

--- scorecard.py
+++ scorecard.py
@@ -2,22 +2,21 @@ from tabulate import tabulate
 
 
 class Scorecard:
-    def __init__(self, scorecard):
+    def __init__(self, scorecard: list[list[str]]):
         self.scorecard = scorecard
 
-    def show_scorecard(self):
-        '''
+    def show_scorecard(self) -> None:
+        """
         Returns the scorecard as a table
-        '''
+        """
         print(tabulate(self.scorecard, tablefmt="pretty"))
-        # return tabulate(self.scorecard)
 
-    def update_scorecard(self, character, search_card):
-        '''
+    def update_scorecard(self, character: str, search_card: str) -> None:
+        """
         Takes the shown card and the character who showed it and updates the
         scorecard in the position of the intersect between that card and
         character.
-        '''
+        """
         # What's the index of the character who showed the card?
         char_num = self.scorecard[0].index(character)
 
@@ -29,33 +28,3 @@ class Scorecard:
 
         # Add a cross in the appropriate place
         self.scorecard[list_index][char_num] = "x"
-
-        # card_num = self.scorecard.index(search_card)
-
-        # print(char_num)
-        # print(list_index)
-        # print(tabulate(self.scorecard, tablefmt="pretty"))
-
-
-# scorecard_test = Scorecard(scorecard)
-
-# scorecard_test.show_scorecard()
-
-# char = "Miss Scarlett"
-# char2 = "Colonel Mustard"
-# char3 = "Mrs White"
-# char4 = "Reverend Green"
-
-# card = "Revolver"
-# card2 = "Conservatory"
-# card3 = "Dagger"
-# card4 = "Kitchen"
-# card5 = "Ball Room"
-# card6 = "Mrs White"
-
-# scorecard_test.update_scorecard(char, card)
-# scorecard_test.update_scorecard(char2, card2)
-# scorecard_test.update_scorecard(char3, card3)
-# scorecard_test.update_scorecard(char4, card4)
-# scorecard_test.update_scorecard(char3, card5)
-# scorecard_test.update_scorecard(char2, card6)
