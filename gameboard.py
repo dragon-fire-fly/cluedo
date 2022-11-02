084ca633d93da83abf3de7093d41a1da4b05507c
Emily Knight <ek93@hotmail.co.uk>
2022-11-02 22:47:04 +0100

    Remove redundant code

--- gameboard.py
+++ gameboard.py
@@ -1,38 +1,4 @@
-# import built in modules
-import random
-
-
-import setup
-
-# Gameboard
-
-
-def number_input_validation(no_options):
-    """
-    Takes a number as input and prompts the user to choose an option.
-    If the user input is a number between 1 and the input, returns the number.
-    If the user input is "i" or "I", show the scorecard.
-    """
-    while True:
-        user_ans = input(
-            f"\nEnter 1 - {no_options} to choose a room or enter 'I' to view "
-            "the investigation card: "
-        ).strip()
-        if user_ans.isnumeric():
-            for num in range(1, no_options + 1):
-                if user_ans == str(num):
-                    return int(user_ans)
-            print(
-                f"Sorry, '{user_ans}' is not a valid input, please enter "
-                f"'I' or a number between 1 and {no_options}."
-            )
-        elif user_ans == "i" or user_ans == "I":
-            setup.scorecard.show_scorecard()
-        else:
-            print(
-                f"Sorry, '{user_ans}' is not a valid input, please enter "
-                f"'I' or a number between 1 and {no_options}."
-            )
+from validation import (number_input_validation)
 
 
 class Gameboard:
@@ -58,7 +24,6 @@ class Gameboard:
         total_distance = row_diff + column_diff
         return int(total_distance)
 
-    # player or run.py
     def room_distances(self) -> dict[str, int]:
         """Calls the calculate_distances() function to calculate the distance
         between the player's current location and each of the rooms on the
@@ -84,19 +49,12 @@ class Gameboard:
     def which_room(self) -> str:
         """Takes the current space of a player and returns which room they
         are in. If player is not in a room, returns hallway."""
-        a = self.player_location[0]
-        b = self.player_location[1]
         for room, space in self.rooms.items():
-            if a == space[0] and b == space[1]:
+            if self.player_location[0] == space[0] and\
+                    self.player_location[1] == space[1]:
                 return room
         return "hallway"
 
-    # def which_coordinates(self, room):
-    #     """Takes the current room of a player and returns the co-ordinates
-    #       for that room. If player is not in a room, returns None."""
-    #     return self.rooms[room]
-
-    # needs a validation decorator
     def choose_room(self):
         """Collects and prints information about which room the player
         is currently in, the outcome of the die roll and the number of
@@ -104,7 +62,6 @@ class Gameboard:
         room the player wants to move to."""
 
         current_room = self.which_room()
-        # print(f"You are currently in the {current_room}.")
         print("=" * 40)
         print(f"The rooms are the following distances: ")
         i = 0
@@ -115,7 +72,6 @@ class Gameboard:
                 passageway_room = self.check_for_secret_passageway(
                     current_room
                     )
-                # print(f"passageway_room: {passageway_room}")
                 if passageway_room:
                     room_distances[passageway_room] = 0
 
@@ -135,7 +91,6 @@ class Gameboard:
         user_room_choice = number_input_validation(9)
         desired_room = room_options.pop(user_room_choice)
         return (desired_room, room_distances)
-        # self.move(player, desired_room, die_roll, room_distances)
 
     def check_for_secret_passageway(self, current_room: str):
         """
